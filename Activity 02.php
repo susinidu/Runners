430b7e21882f71c7b59dc702e2e5ce1cdb7387be
Sadeepa <susinidusadeepa@gmail.com>
2022-09-26 10:51:34 +0530

    Create Form to add Runners details.

---
+++ Activity 02.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runners</title>
+</head>
+<body>
+    <h2>Data Input</h2>
+
+        <form action="" method="post">
+            <div class="form group">
+            <label>Runner Name :</label>
+            <dd><input type = "text" name ="studentname" id="studentname" class="form control"></dd>
+        </div><br>
+        <div class="form group">
+            <label>Radius(m) :</label>
+            <dd><input type = "number" name="studentage" id="studentage" class="form control"></dd>
+        </div><br>
+        <div class="form group">
+            <label>Start Time :</label>
+            <dd><input type = "number" name="studentage" id="studentage" class="form control"></dd>
+        </div><br>
+        <div class="form group">
+            <label>End Time :</label>
+            <dd><input type = "number" name="studentage" id="studentage" class="form control"></dd>
+        </div><br>
+        <div class="form group">
+            <label>Number Of Laps:</label>
+            <dd><input type = "number" name="studentage" id="studentage" class="form control"></dd>
+        </div><br>
+        <input type="submit" class="submit">
+     </form><br>
+     
+</body>
+</html>
