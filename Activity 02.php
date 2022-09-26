cfd41b5ceea33718cdeca82b161364ccb9a24457
Sadeepa <susinidusadeepa@gmail.com>
2022-09-26 23:42:50 +0530

    Create Structure of Report Table and fetch raw data into the table

--- Activity 02.php
+++ Activity 02.php
@@ -39,9 +39,42 @@
             <dd><input type = "number" name="numberoflaps" id="numberoflaps" class="form control"></dd>
         </div><br>
         <input type="submit" class="submit">
-     </form><br>
+     </form>
 
-     
+     <?php
+    $connection = mysqli_connect("localhost","root","","RUNNERS");
+?>
+
+     <h2>Report</h2>
+
+        <table width =80% >
+            <tr>
+                    <th>Runner Id</th>
+                    <th>Runner</th>
+                    <th>Radius</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Number Of Laps</th>
+
+            </tr>
+
+                <?php
+                    $sql ="SELECT * FROM `RunnersData`";
+                    $result = mysqli_query($connection,$sql);
+                    while ($row =mysqli_fetch_assoc($result)) {
+
+                    echo '<tr>
+                    <td>'.$row['Runner_Id'].'</td>
+                    <td>'.$row['Runner_Name'].'</td>
+                    <td>'.$row['Radius'].'</td>
+                    <td>'.$row['Start_Time'].'</td>
+                    <td>'.$row['End_Time'].'</td>
+                    <td>'.$row['Number_Of_Laps'].'</td>
+            </tr>';
+                    }
+                ?>
+
+        </table>
      
 </body>
 </html>
